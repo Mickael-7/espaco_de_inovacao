c9104243e302bbbbb0f1f91c4e54e640c806461e
Mickael-7 <mickael.albuquerque@upe.br>
2025-06-24 11:28:29 -0300

    feat: add parceiros data for empresarial, academico, and institucional categories

---
+++ espaco-inovacao/src/data/parceiros.ts
@@ -0,0 +1,20 @@
+// Dados simulados (substitua pelas reais ou API futuramente)
+export const parceirosEmpresariais = [
+  { nome: "Sebrae" , logoUrl: "https://sebrae.com.br/Sebrae/Portal%20Sebrae/UFs/BA/Imagens/topo_Sebrae.png", bgColor: "#3E4CD2" },
+  { nome: "Comunidadde Sete Colinas" , logoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSWt4HX8BzIavO1UvFV4f0CViIrPfbz9u378w&s", bgColor: "#F9FAFB" },
+  { nome: "Stellantis" , logoUrl: "https://revistacarro.com.br/wp-content/uploads/2020/11/Logotipo-Stellantis_1.jpg", bgColor: "#243881" },
+  { nome: "Accenture" , logoUrl: "https://yt3.googleusercontent.com/2yc-aIPPXJ5FiEnXi0CZ4NM7v2pqHy_83ZxNMi53NLt6LglIU5HWb1n9JnmgQP1nAwBJSrRifRw=s900-c-k-c0x00ffffff-no-rj", bgColor: "#F9FAFB"},
+  { nome: "Google" , logoUrl: "https://t.ctcdn.com.br/lvns56iaSMyHvyTur4JeYS_NYeY=/i606944.png",  bgColor: "#F9FAFB" },
+];
+
+export const parceirosAcademicos = [
+  { nome: "Universidade de Pernambuco" , logoUrl: "https://www.upe.br/images/industrix/logo-nova4.png", bgColor: "#F9FAFB" },
+  { nome: "Instituto de Tecnologia Edson Mororó Moura" , logoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQU6XIlzjYP2gglIzd-CeXdoWqpulon1sOYJg&s", bgColor: "#E2E0E0" },
+];
+
+export const parceirosInstitucionais = [
+  { nome: "Crea-PE" , logoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrVaVEegY2IIU4hOUWFV2Kk43b1ynV9MuCGQ&s", bgColor: "#0159A0" },
+  { nome: "Semas-PE" , logoUrl: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSBzShtS9bINfmBuLSE1nPr4utYnXLACSR70A&s" ,  bgColor: "#F9FAFB"},
+  { nome: "Cetene" , logoUrl: "https://www.gov.br/mcti/pt-br/composicao/rede-mcti/CETENE.png/@@images/0add997c-65a9-4820-a7dc-43a50ff7f44f.png", bgColor: "#F9FAFB" },
+  { nome: "Prefeitura De Garanhuns-pe" , logoUrl: "https://lh5.googleusercontent.com/proxy/voycjqCHy8pZf5_Oirf4ML8U-ItEeI07H8Lzea3KlgmreEEM0MzHAIahBoeyfb_YN2sPNJAZlmiETPGa1yiEX_P9nmeTAK_S-1pKfsl-h2jp7w3QjfA0KiHAuQ", bgColor: "#F9FAFB"  },
+];
